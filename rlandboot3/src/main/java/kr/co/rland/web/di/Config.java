bc31bc395ca389b77feaa3f5d0c966151b853a3c
LDH <wuzget0508@naver.com>
2023-03-10 18:23:35 +0900

    "Config자바 고치기"

--- rlandboot3/src/main/java/kr/co/rland/web/di/Config.java
+++ rlandboot3/src/main/java/kr/co/rland/web/di/Config.java
@@ -21,15 +21,15 @@ import kr.co.rland.web.repository.jdbc.JdbcMenuRepository;
 //@Configuration
 public class Config {
 	
-	@Bean
-	public JdbcMenuRepository repository() { //객체를 만들기위한 코드를 만들어내지만 스프링처리기가 콩자
-		//원래는 객체를 만들어서 return하는 방식
-		//스프링에서는 콩자루에 담겨줄거기 때문에 객체를 만드는 것이 아닌 함수명만 지어준 것이 된다.
-		//객체를 만들어달라는 xml과 똑같은 방식이다.
-		
-				
-		return new JdbcMenuRepository();
-	}
+//	@Bean
+//	public JdbcMenuRepository repository() { //객체를 만들기위한 코드를 만들어내지만 스프링처리기가 콩자
+//		//원래는 객체를 만들어서 return하는 방식
+//		//스프링에서는 콩자루에 담겨줄거기 때문에 객체를 만드는 것이 아닌 함수명만 지어준 것이 된다.
+//		//객체를 만들어달라는 xml과 똑같은 방식이다.
+//		
+//				
+//		return new JdbcMenuRepository();
+//	}
 	@Bean
 	public List list() {
 		
